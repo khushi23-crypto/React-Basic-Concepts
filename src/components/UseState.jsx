d09ce5ef9ce178ccf47feb1e2d124662922b4ffe
Kajal Saini <sizukabts91@gmail.com>
2025-08-28 12:25:22 +0530

    useState concept  with multiple variables

--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -3,21 +3,46 @@ import { useState } from 'react';
 import './UseState.css'
 
 function UseState() {
-  const [color, setColor] = useState('Red')
-
+  const[color,setColor]=useState('Red');
+  const [car, setCar] = useState({
+    brand:'Ferrari',
+    model:'Rome',
+    year:'2023',
+    color:'red'
+  });
 
   const changeColor = () => {
 
     setColor('Blue')
   }
+  const changeBrand=()=>{
+    setCar((prev)=>{
+      return {...prev , color: "blue",brand: "BMW", model: "India", year: "2025" }
+    })
+  }
   return (
-
     <>
-      <h1 className='head'>My favourite color is {color}!</h1>
-      <div className="container">
-      <button type='button' className='btn' onClick={changeColor}>Blue</button>
+    <div className="cards-wrapper">
+    <div className="card-container">
+      <div className="card-body">
+        <h1 className='head'>My favourite color is {color}!</h1>
+        <p>This is content for the single variable.</p>
+        <button type='button' className='btn' onClick={changeColor}>Blue</button>
+      </div>,
+      <div className="card-body">
+        <h1 className='head'>My {car.brand} !</h1>
+        <p>It is a {car.color} {car.model} from {car.year}</p>
+        <button type='button' className='btn' onClick={changeBrand}>Blue</button>
+      </div>,
+      <div className="card-body">
+       <h1 className='head'>My favourite color is {color}!</h1>
+        <p>This is content for the first card.</p>
+        <button type='button' className='btn' onClick={changeColor}>Blue</button>
+      </div>
+      </div>
       </div>
     </>
+
   )
 }
 
