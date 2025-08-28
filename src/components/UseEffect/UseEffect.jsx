01d726501c3a4feedf80ef8f0231a19f257c507f
Kajal Saini <sizukabts91@gmail.com>
2025-08-28 15:08:33 +0530

    run only 1st render,run everytime when count updated, contain multiple dependencies

--- src/components/UseEffect/UseEffect.jsx
+++ src/components/UseEffect/UseEffect.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react'
 
 function UseEffect() {
     const [count, setCount] = useState(0);
-
+    const [counter, setCounter] = useState(0);
+    const [counting, setCounting] = useState(0);
+    const [counts, setCounts] = useState(0);
     //first -> side-effect fuction
     //second -> clea n-up function
     //third -> comma seperated dependencies isme jaise array ke andr count likhenge to count jb bhi update hoga ye effect fun chlega 
@@ -12,16 +14,40 @@ function UseEffect() {
     //variation :2
     //runs on only first render
     //variation: 3
-    //runs on limited time
+    //runs on only when something value is updated
 
+    //variation dependencies
+
+
+    /*useEffect(() => {
+        alert("I will run on each render")
+    })*/
+
+    /*useEffect(() => {
+        alert("I will run on each render")
+    },[])*/
+
+
+    /*useEffect(() => {
+        alert('I will run every time when count is updated')
+    }, [counting])*/
 
     useEffect(() => {
-        alert("I will run on each render")
-    })
+     alert("I will run every time when count/counts is updated")
+    }, [count,counts,counting])
     
+    function handleCounter() {
+        setCounter(counter + 1)
+    }
 
     function handleClick() {
         setCount(count + 1);
+    }
+    function handleCounting() {
+        setCounting(counting + 1)
+    }
+    function handleCounts() {
+        setCounts(counts +1)
     }
 
     return (
@@ -31,6 +57,21 @@ function UseEffect() {
                     <button type='button' className='btn' onClick={handleClick}>Click me</button>
                     <h1 className='head'>Count : {count}</h1>
                     <p>Runs on every render</p>
+                </div>
+                <div className="card-body">
+                    <button type='button' className='btn' onClick={handleCounter}>Click me</button>
+                    <h1 className='head'>Counter : {counter}</h1>
+                    <p>Runs on only first render</p>
+                </div>
+                <div className="card-body">
+                    <button type='button' className='btn' onClick={handleCounting}>Click me</button>
+                    <h1 className='head'>Counting : {counting}</h1>
+                    <p>Runs on only when update the count render</p>
+                </div>
+                <div className="card-body">
+                    <button type='button' className='btn' onClick={handleCounts}>Click me</button>
+                    <h1 className='head'>Counts : {counts}</h1>
+                    <p>Runs on only when update the count/counts/counting render</p>
                 </div>
             </div>
         </div>
